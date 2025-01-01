240aaeee712fb902312f11f0b8664b83fad35c1b
Shreeya Paudel <shreeya.paudel.8e@gmail.com>
2025-01-01 17:42:51 +0545

    works

---
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+import javaapplication1.firstswing.view.StartScreen;
+
+/**
+ *
+ * @author TUF
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StartScreen ss = new StartScreen();
+        ss.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
